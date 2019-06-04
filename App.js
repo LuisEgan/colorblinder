1c8fab354d961ab1d723ad4b69f2554db1340aba
ldev <luisdegan10@hotmail.com>
2019-06-04 14:23:18 -0400

    home

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from "./screens/Home";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! :p</Text>
-      </View>
-    );
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
+
+export default createAppContainer(AppNavigator);
