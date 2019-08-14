a94f32a96a309af2cce692c2077bcd6896b136fa
LDEV <luisdegan10@hotmail.com>
2019-08-14 18:10:04 -0400

    ayy

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./screens/Home";
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Home
-  }
-});
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+  },
+  {
+    initialRouteName: "Home",
+    headerModer: "none",
+  },
+);
 
 export default createAppContainer(AppNavigator);
